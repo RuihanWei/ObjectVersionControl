8b02250e64c486a1ab9ed1a4a49fcd5b567d57f1
RuihanWei <ruihanwei7@gmail.com>
2020-05-12 22:19:57 -0400

    adding image upload

--- Controller/MainController.py
+++ Controller/MainController.py
@@ -22,35 +22,35 @@ app = Flask(__name__, template_folder=template_folder, static_url_path="/static"
 username = ""
 
 
-@app.route('/')
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-  global username
-  error = None
-  if request.method == 'POST':
-    if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-      print('invalid')
-      error = 'Invalid Credentials. Please try again.'
-      return redirect(url_for('selection'))
-    else:
-      print('valid')
-      print(username)
-      username = request.form['username']
-      return redirect(url_for('selection'))
-  # todo: incorporate error into HTML
-  return render_template('/LoginTemplate.html', error=error)
-
-
-@app.route('/selection')
-def selection():
-  # todo: retrieve from database
-  sessions = []
-  session1_datetime = datetime.datetime(2019, 4, 1, 23, 58, 25)
-  session2_datetime = datetime.datetime(2019, 4, 3, 1, 26, 18)
-  sessions.append(Session(str(session1_datetime), "desk area"))
-  sessions.append(Session(str(session2_datetime), "desk area"))
-  print(username)
-  return render_template('/SelectionView.html', sessions=sessions, username=username)
+# @app.route('/')
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#   global username
+#   error = None
+#   if request.method == 'POST':
+#     if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+#       print('invalid')
+#       error = 'Invalid Credentials. Please try again.'
+#       return redirect(url_for('selection'))
+#     else:
+#       print('valid')
+#       print(username)
+#       username = request.form['username']
+#       return redirect(url_for('selection'))
+#   # todo: incorporate error into HTML
+#   return render_template('/LoginTemplate.html', error=error)
+#
+#
+# @app.route('/selection')
+# def selection():
+#   # todo: retrieve from database
+#   sessions = []
+#   session1_datetime = datetime.datetime(2019, 4, 1, 23, 58, 25)
+#   session2_datetime = datetime.datetime(2019, 4, 3, 1, 26, 18)
+#   sessions.append(Session(str(session1_datetime), "desk area"))
+#   sessions.append(Session(str(session2_datetime), "desk area"))
+#   print(username)
+#   return render_template('/SelectionView.html', sessions=sessions, username=username)
 
 
 @app.route('/selection/api')
@@ -91,17 +91,27 @@ def sessionRequestAPI():
 
 
 
-@app.route('/upload')
+#@app.route('/upload')
+@app.route('/')
 def upload_file():
   return render_template('/upload.html')
 
 
-@app.route('/uploader', methods=['GET', 'POST'])
+@app.route('/uploader1', methods=['GET', 'POST'])
 def uploader():
   if request.method == 'POST':
     f = request.files['file']
-    f.save(secure_filename(f.filename))
-    return 'file uploaded successfully'
+    #f.save(secure_filename(f.filename))
+    f.save(os.path.join("../View/static", "image1.jpg"))
+    return render_template('/upload_image1.html')
+
+@app.route('/uploader2', methods=['GET', 'POST'])
+def uploader_image1():
+  if request.method == 'POST':
+    f = request.files['file']
+    f.save(os.path.join("../View/static", "image2.jpg"))
+    return render_template('/upload_image2.html')
+
 
 
 if __name__ == '__main__':
